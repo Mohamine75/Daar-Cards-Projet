596e48f944a3630f8a7a3d9befe2b5e7153e3c2d
Amine <brahitiamine@hotmail.fr>
2024-10-27 02:25:03 +0200

    Frontend fini :)

--- frontend/MarketPlace.js
+++ frontend/MarketPlace.js
@@ -683,7 +683,7 @@ async function displayAvailableCards() {
             </div>
             <div class="card-content">
               <p class="title is-5">${card.cardType.nom}</p>
-              <p>Price: ${card.cardType.prix} ETH</p>
+              <p class="price-text">Price: ${card.cardType.prix} ETH</p>
               <button class="button is-primary" onclick="buyCard(${index}, ${card.cardType.prix})">Buy Now</button>
             </div>
           </div>
